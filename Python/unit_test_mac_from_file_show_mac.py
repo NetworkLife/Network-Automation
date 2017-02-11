f5039870a1ca525050e10db49fb61bef1e7631a8
Benoit <benoit@bgoncalves.fr>
2017-02-11 11:25:50 +0100

    add
    add verification of the mac / vlan association

--- Python/unit_test_mac_from_file_show_mac.py
+++ Python/unit_test_mac_from_file_show_mac.py
@@ -2,6 +2,20 @@
 # Open a file with a "show mac address-table" and extract VLAN, MAC address, Port
 # version: 0.1
 
+'''
+Create the following table from a "show mac address-table"
++--------+-----------+------+-----+------+-------------------------------------+--------------------+
+| SWITCH | SWITCH_IP | VLAN | MAC | PORT | Type of Mac (if known) : AP, IMP... | Good vlan ? (BOOL) |
++--------+-----------+------+-----+------+-------------------------------------+--------------------+
+'''
+
+# Declare variables
+AP_CISCO_MAC = "00fe"
+AP_CISCO_VLAN = 895
+
+PRINTER_MAC = "00ce"
+PRINTER_VLAN = 200
+
 import re
 
 # Init two dimension list
@@ -46,7 +60,44 @@ for line in hand:
     else:
         row.append("")
 
+	#VERIFY if the mac address is a known type
+    if (mac[:4] == AP_CISCO_MAC):
+        row.append("AP_CISCO")
+    elif (mac[:4] == PRINTER_MAC):
+        row.append("PRINTER")
+    else:
+        row.append("")
+
+	#VERIFY if the mac address match the good vlan
+    if (mac[:4] == AP_CISCO_MAC) and (vlan[0] != AP_CISCO_VLAN):
+        row.append(False)
+    elif (mac[:4] == AP_CISCO_MAC) and (vlan[0] == AP_CISCO_VLAN)
+        row.append(True)
+    elif (mac[:4] == PRINTER_MAC) and (vlan[0] != PRINTER_VLAN):
+        row.append(False)
+    elif (mac[:4] == PRINTER_MAC) and (vlan[0] == PRINTER_VLAN)
+        row.append(True)
+    else:
+        row.append("")
+
     twod_list.append(row)
+
+'''
+Print the table
+'''
+for line in twod_list:
+    print(line)
+
+'''
+Generate the config for the wrongly configured ports
++--------+-----------+------+-----+------+-------------------------------------+--------------------+
+| SWITCH | SWITCH_IP | VLAN | MAC | PORT | Type of Mac (if known) : AP, IMP... | Good vlan ? (BOOL) |
++--------+-----------+------+-----+------+-------------------------------------+--------------------+
+
+ if MAC is AP and VLAN is NOK, then reconfigure
+ if MAC is PRINTER and VLAN is NOK, then reconfigure
+
+'''
 
 for line in twod_list:
     print(line)
