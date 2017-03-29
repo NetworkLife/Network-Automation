835e15e78400be29773591f74b6adcdb7f9d7193
Ben <benoit@bgoncalves.fr>
2017-03-29 10:18:30 +0200

    Add files via upload

--- inventory-infos-from-multiple-switches-to-csv/netmiko_multiprocess_inventory_infos.py
+++ inventory-infos-from-multiple-switches-to-csv/netmiko_multiprocess_inventory_infos.py
@@ -8,8 +8,16 @@ Create the following table from a "show inventory" on multiple switches
 '''
 
 import textfsm
+import sys
+
+# Catch Paramiko warnings about libgmp and RandomPool
+import warnings
+with warnings.catch_warnings(record=True) as w:
+    import paramiko
+
 import multiprocessing
 from datetime import datetime
+
 import netmiko
 from netmiko.ssh_exception import NetMikoTimeoutException, NetMikoAuthenticationException
 
@@ -18,30 +26,33 @@ from DEVICE_CREDS import all_devices
 
 def normalise_show_inventory_output(results):
     for a_dict in results:
-        for identifier,v in a_dict.items():
-            (out_string) = v
-            print (out_string);
-            print ('-'*20);
-            print ('\n');
+        for identifier,v in iter(a_dict.items()):
+            (success, out_string) = v
+            if success:
+                print ('{0}\n{1}'.format(identifier,out_string));
+                print ('\n');
+                print ('-'*40);
+                print ('\n');		
+    print	
 
 def show_inventory(a_device, mp_queue):
+    debug = True
     return_data = {}
-    identifier = '{ip}'.format(**a_device)
-    
+	
+    show_inventory_command = 'show inventory '
+    SSHClass = netmiko.ssh_dispatcher(a_device['device_type'])
+
     try:
-        SSHClass = netmiko.ssh_dispatcher(a_device['device_type'])
-		
         net_connect = SSHClass(**a_device)
-        show_inventory_command = 'show inventory'
+        hostname = net_connect.find_prompt()
+        identifier = hostname
+        identifier += '{ip}'.format(**a_device)
         show_inventory = net_connect.send_command_timing(show_inventory_command)
+        return_data[identifier] = (True, show_inventory) 
     except (NetMikoTimeoutException, NetMikoAuthenticationException) as e:
         return_data[identifier] = (False, e)
-
-    # Add data to the queue (for parent process)
-    mp_queue.put(return_data)
-    return None
-
-    return_data[identifier] = (show_inventory)
+        return None
+       
     mp_queue.put(return_data)
 		
 def main():
@@ -50,7 +61,7 @@ def main():
     processes = []
     print ("\nStart time: " + str(datetime.now()));
 
-	# For each device, send a "show interfaces"
+	# For each device, send a "show inventory"
     for a_device in all_devices:
         p = multiprocessing.Process(target=show_inventory, args=(a_device, mp_queue))
         processes.append(p)
@@ -58,16 +69,28 @@ def main():
         p.start()	
 
     # wait until the child processes have completed
-    for p in processes:
-        p.join()
-		
-    # retrieve all the data from the "show interfaces" queue
+#    for p in processes:
+#        p.join()
+	
+    # retrieve all the data from the "show inventory" queue
     results = []
     for p in processes:
         results.append(mp_queue.get())
-		
+	
 	# add '-------------' between each "show interface" output
-    raw_text_data = normalise_show_interface_output(results)
+    #raw_text_data = normalise_show_inventory_output(results)
+     	
+    orig_stdout = sys.stdout
+    fichier = open("show_inventory.txt", "w", encoding='utf-8')
+    sys.stdout = fichier
+    fichier.write(str(normalise_show_inventory_output(results)))
+    sys.stdout = orig_stdout
+    fichier.close()
+	
+	# Load the input file to a variable
+    input_file = open("show_inventory.txt", encoding='utf-8')
+    raw_text_data = input_file.read()
+    input_file.close()
 	
 	# textFSM the output
     # The argument 'template' is a file handle and 'raw_text_data' is a 
